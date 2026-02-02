4fba6d0af53e6159faea0e17b21a858624fb807d
DJAKALET Jean Jacques Yao <jeanjackdjakale@gmail.com>
2026-02-02 18:50:55 +0000

    Refonte de big-logo.js

--- js/big-logo.js
+++ js/big-logo.js
@@ -1,13 +1,22 @@
 const bigLogo = document.querySelector(".big-logo");
 
-const screenWidth = window.innerWidth;
-
 const logoImg = document.createElement("img");
 logoImg.src = "img/logo-sigex-mini.jpeg";
 logoImg.alt = "big logo";
-if (screenWidth >= 1000) {
-  logoImg.style.width = `${screenWidth - 300}px`;
-} else {
-  logoImg.style.width = `${screenWidth - 50}px`;
-}
 bigLogo.appendChild(logoImg);
+
+function resizeLogo() {
+  const screenWidth = window.innerWidth;
+
+  if (screenWidth >= 1000) {
+    logoImg.style.width = `${screenWidth - 300}px`;
+  } else {
+    logoImg.style.width = `${screenWidth - 50}px`;
+  }
+}
+
+// Au chargement
+resizeLogo();
+
+// À chaque changement de taille d’écran
+window.addEventListener("resize", resizeLogo);
